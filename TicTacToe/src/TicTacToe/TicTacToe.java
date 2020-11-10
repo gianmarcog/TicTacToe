01912d6668a3136d3be5473248a3cdc97d54f1e4
gianmarcog <marco.giretti@web.de>
2020-11-11 00:33:08 +0100

    Added CPU Player

--- TicTacToe/src/TicTacToe/TicTacToe.java
+++ TicTacToe/src/TicTacToe/TicTacToe.java
@@ -1,5 +1,8 @@
 package TicTacToe;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TicTacToe {
 	
 	public static void main(String[] args) {
@@ -12,7 +15,23 @@ public class TicTacToe {
 				{' ', '|', ' ', '|', ' '}
 		};
 		printGameBoard(gameBoard);
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your placement (1-9): ");
+		int playerPos = scan.nextInt();
+		
+		System.out.println(playerPos);
+		
+		placePiece(gameBoard, playerPos,"player");
+		
+		Random rand = new Random();
+		int cpuPos = rand.nextInt(9) + 1;
+		placePiece(gameBoard, cpuPos,"cpu");
+		
+		printGameBoard(gameBoard);
 	}
+	
+	
 	
 	public static void printGameBoard(char [][] gameBoard) {
 		
@@ -23,6 +42,49 @@ public class TicTacToe {
 				System.out.println();
 		}
 		
+	}
+	
+	public static void placePiece(char [][] gameBoard, int pos, String user) {
+		
+		char symbol = ' ';
+		
+		if(user.equals("player")) {
+			symbol = 'X';
+		} else if(user.equals("cpu")) {
+			symbol = 'O';
+		}
+		
+		switch(pos) {
+		case 1: 
+			gameBoard[0][0] = symbol;
+			break;
+		case 2: 
+			gameBoard[0][2] = symbol;
+			break;
+		case 3: 
+			gameBoard[0][4] = symbol;
+			break;
+		case 4: 
+			gameBoard[2][0] = symbol;
+			break;
+		case 5: 
+			gameBoard[2][2] = symbol;
+			break;
+		case 6: 
+			gameBoard[2][4] = symbol;
+			break;
+		case 7: 
+			gameBoard[4][0] = symbol;
+			break;
+		case 8: 
+			gameBoard[4][2] = symbol;
+			break;
+		case 9: 
+			gameBoard[4][4] = symbol;
+			break;
+		default:
+			break;
+		}
 	}
 
 }
